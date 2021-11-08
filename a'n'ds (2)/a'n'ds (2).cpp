871040d256d8ce135327d7fc88acf20b8b12c6b9
Горюнов Дмитрий <dsgoryunov@stud.etu.ru>
2021-11-08 11:44:54 +0300

    Add project files.

---
+++ a'n'ds (2)/a'n'ds (2).cpp
@@ -0,0 +1,10 @@
+// a'n'ds (2).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*some new commit*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
